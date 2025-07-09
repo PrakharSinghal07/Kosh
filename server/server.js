788debea3a5c8a68258046389e990ee3a2e96010
Prakhar Singhal <singhalprakhar.rishu@gmail.com>
2025-07-10 01:47:39 +0530

    changed frontend url in cors configuration

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ cloudinary.config({
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://bookworm-thelibrary-management-system.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
